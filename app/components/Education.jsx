4a0deb2c7db194f80b228b471b1bf80d901e7c1a
janfincke <janfincke1@gmail.com>
2017-04-20 00:12:56 +0300

    Added skeleton for education info

--- app/components/Education.jsx
+++ app/components/Education.jsx
@@ -1,19 +1,232 @@
 import React, {Component} from 'react';
 
+const headerStyle = {}
+const pStyle = {}
+const hrStyle = {}
+
 export default class Education extends Component {
     render() {
         return (
+          <div className="row">
+            <div className="main-text col-xs-6">
+              <h2>VIA University College</h2>
+              <p>
+                  Kansainvälinen opiskelijavaihto<br />
+                  ICT Engineering<br />
+                  08/2016 - 12/2016<br />
+
+              </p>
+            </div>
+            <div className="main-text-list col-xs-6">
+                <ul>
+                  <li>
+                    <ul>
+                    <li>
+                      Single-page Web Applications
+                    </li>
+                    <li>
+                      Advanced .NET Programming
+                    </li>
+                    <li>
+                      Linux Server Administration and Open Source
+                    </li>
+                    <li>
+                      ERP Systems SAP ABAP/4 Programming
+                    </li>
+                    <li>
+                      Database Internals and Tuning (Oracle)
+                    </li>
+                    <li>
+                      Lego Robot Lab
+                    </li>
+                    </ul>
+                  </li>
+
+
+                </ul>
+
+            </div>
+            <div className="main-text">
+                <br />
+                <hr/>
+                <br />
+            </div>
+            <div className="main-text">
+                <h2>Hämeen ammattikorkeakoulu</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis elit dui. Integer mollis sem arcu, quis egestas nisl posuere vel. Suspendisse potenti. Aliquam vitae est metus. In hac habitasse platea dictumst. Nulla nec pharetra erat. Donec tincidunt luctus nulla, vitae commodo leo ullamcorper et. Proin in condimentum mi, quis tincidunt eros. Mauris est augue, efficitur in arcu et, feugiat vehicula mi. Nam scelerisque velit eget libero accumsan, eget interdum nibh commodo. Quisque varius volutpat bibendum. Sed ut pharetra leo. Proin condimentum sem sapien, vitae varius eros vestibulum nec. Cras facilisis rhoncus sem, finibus lacinia odio cursus ut. Fusce pellentesque blandit arcu in facilisis. Nulla finibus purus non lobortis scelerisque.
-
-                    Nunc dignissim risus ac viverra commodo. Fusce ac facilisis massa. Nulla facilisi. Nullam eu rhoncus orci. Maecenas lacus dui, convallis id consectetur a, imperdiet et nisi. Nunc et nunc sit amet nibh dapibus ullamcorper in eget quam. Proin luctus lorem sit amet justo venenatis finibus. Curabitur ornare nisi sit amet odio ornare, sed rutrum orci venenatis. Aenean eu dui eleifend, blandit lectus a, mollis sapien. Aenean iaculis imperdiet convallis. Curabitur venenatis, enim ac sollicitudin consectetur, enim mauris sollicitudin dui, a consectetur erat sem in mauris. Cras sollicitudin eleifend eros vitae consequat. Interdum et malesuada fames ac ante ipsum primis in faucibus. Praesent porta mauris ac mi commodo, et pretium risus aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean et purus id risus dictum lacinia sed eget felis.
-
-                    Donec lacus nunc, accumsan vitae condimentum ut, faucibus eu magna. Ut fermentum auctor interdum. Nulla ac rhoncus nisl, sed commodo tortor. Suspendisse tortor eros, pellentesque vel lorem quis, luctus vehicula sapien. Vivamus nec tellus a dui pellentesque porttitor ac sit amet arcu. Nulla aliquam, lorem non egestas vestibulum, felis tortor blandit lacus, non imperdiet est purus ut diam. Integer placerat placerat est nec fringilla. Nulla facilisi. Mauris sollicitudin iaculis risus vel rhoncus. Donec fringilla risus ut lectus accumsan consequat. Curabitur cursus tempor erat, non fringilla ipsum. Proin aliquam, sapien nec laoreet facilisis, odio nisi sagittis libero, vitae pulvinar augue urna id enim. Quisque rutrum felis sed ultrices lobortis. In hac habitasse platea dictumst. Nam lobortis mattis nisl, eu auctor odio mattis eu. Nunc consectetur luctus leo, vel tincidunt mi interdum et.
-
-                    Quisque finibus mauris sed leo finibus, luctus tempor erat vulputate. Vivamus tincidunt convallis nibh, id condimentum diam eleifend at. Nam eget ultrices libero. Donec leo massa, bibendum in magna blandit, cursus lobortis dui. Etiam molestie purus quis velit congue, a imperdiet lacus pulvinar. Praesent ut venenatis leo. Phasellus ac cursus turpis, vitae mattis eros. Nunc consequat consectetur justo, a bibendum erat sollicitudin id. Vestibulum consequat dolor interdum purus iaculis, eget dictum sem accumsan. Phasellus eleifend dui nec purus ullamcorper rhoncus. Donec ac bibendum ex. Pellentesque rhoncus euismod mi quis malesuada. Nulla at risus odio. Mauris convallis leo eu tempus rutrum.
-
-                    In hac habitasse platea dictumst. Quisque suscipit a justo et gravida. Pellentesque ac suscipit elit. Integer at est venenatis, scelerisque sapien eget, ullamcorper neque. Sed ut pretium libero. Fusce ac turpis lorem. Nam commodo fermentum ipsum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi leo dolor, semper eget magna et, rutrum malesuada urna. Praesent nec malesuada odio, malesuada hendrerit eros. Donec ut metus nunc. Integer non orci tincidunt, condimentum nisl nec, malesuada nisl. Maecenas arcu ante, dignissim eu porta vel, scelerisque nec tellus. Vivamus eget mollis elit. Nam mauris lectus, laoreet nec nisi et, elementum mattis enim. Nulla ac lectus quis tellus cursus sodales quis ac massa.
+                    Tieto- ja viestintätekniikan insinööri<br />
+                    2014 - (2018)<br />
                 </p>
+            </div>
+            <div className="main-text-list">
+                <ul>
+                    <li> Ohjelmointi
+                        <ul>
+                            <li>
+                                Ohjelmoinnin perusteet (Javascript)
+                            </li>
+                            <li>
+                                Olio-ohjelmointi (Java)
+                            </li>
+                            <li>
+                                Kuvauskielten perusteet (HTML/CSS)
+                            </li>
+                            <li>
+                                Tietokantojen suunnittelu ja ohjelmointi (MS Access, MySQL)
+                            </li>
+                            <li>
+                                Asiakas- ja palvelinpuolen web-ohjelmointi (LAMP, Javascript)
+                            </li>
+                            <li>
+                                Grafiikkaohjelmointi
+                            </li>
+                            <li>
+                                Käyttöliittymäohjelmointi (C#)
+                            </li>
+                        </ul>
+                        <br />
+                    </li>
+
+                    <li> Tietojärjestelmät
+                        <ul>
+                            <li>
+                                Tietoverkkojen perusteet (Cisco CCNA1)
+                            </li>
+                            <li>
+                                Reititin- ja kytkinverkot (Cisco CCNA2)
+                            </li>
+                            <li>
+                                Linux-/Unix-hallinnointi
+                            </li>
+                            <li>
+                                Skaalautuvat verkot
+                            </li>
+                            <li>
+                                Hallintasovellukset
+                            </li>
+                            <li>
+                                Analysointi ja monitorointi
+                            </li>
+                            <li>
+                                Digitaalielektroniikka ja sulautetut järjestelmät
+                            </li>
+                            <li>
+                                Elektroniikka
+                            </li>
+                            <li>
+                                Mittaustekniikan perusteet<br />
+                            </li>
+                        </ul>
+                        <br />
+                    </li>
+
+                    <li> Yleissivistävät aineet
+                        <ul>
+                            <li>
+                                Matematiikka
+                            </li>
+
+                            <li>
+                                Mekaniikka
+                            </li>
+                            <li>
+                                Sähkö ja magnetismi
+                            </li>
+                            <li>
+                                Viestintä
+                            </li>
+                            <li>
+                                Ruotsi
+                            </li>
+                            <li>
+                                Englanti
+                            </li>
+
+                        </ul>
+                    </li>
+                </ul>
+
+            </div>
+            <div className="main-text">
+                <br />
+                <hr/>
+                <br />
+            </div>
+            <div className="main-text">
+                <h2>Hyria koulutus Oy</h2>
+                <p>
+                    Tieto- ja tietoliikennetekniikan perustutkinto<br />
+                    2009 - 2012<br />
+                </p>
+            </div>
+            <div className="main-text-list">
+                <ul>
+                    <li> Elektroniikan ja ICT:n perustehtävät
+                        <ul>
+                            <li>
+                                Sähkötekniikan peruskytkentöjen mittaus
+                            </li>
+
+                        </ul>
+                        <br />
+                    </li>
+
+                    <li> Tietokone- ja tietoliikenneasennukset
+                        <ul>
+                            <li>
+                                Asiakaskoneen asennus ja konfigurointi
+                            </li>
+
+                        </ul>
+                        <br />
+                    </li>
+
+                    <li> Palvelinjärjestelmät ja projektityöt
+                        <ul>
+                            <li>
+                                Pienyrityksen tietoverkon asennus ja konfigurointi
+                            </li>
+
+                        </ul>
+                        <br />
+                    </li>
+
+                    <li> Tietokonehuolto
+                        <ul>
+                            <li>
+                                Työasemien ja tulostimien asennus ja verkkoon liittäminen
+                            </li>
+
+                        </ul>
+                        <br />
+                    </li>
+
+
+                    <li> Järjestelmäasennukset
+                        <ul>
+                            <li>
+                                Viestilaitekehikon kokoonpano
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+            <div className="main-text">
+                <br />
+                <hr />
+                <br />
+            </div>
+            <div className="main-text">
+                <h2>Varusmieskoulutus</h2>
+            </div>
+            <div className="main-text-list">
+                <ul>
+                    <li>
+                        Aliupseerikoulutus (tietojärjestelmäasentaja)
+                    </li>
+                </ul>
+            </div>
+        </div>
         );
     }
 }
